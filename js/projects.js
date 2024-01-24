ee242b4f168c74183c6411a37540f1de55d399cf
unknown <ntobekothe2@gmail.com>
2024-01-24 16:08:17 +0200

    Frontend Project (GET Request) done

---
+++ js/projects.js
@@ -0,0 +1,50 @@
+const apiEndpoint = baseUrl + "/api/v1/projects";
+
+
+function addToProjects(projects, data) {
+    let a = document.createElement("a");
+    a.href = data["url"];
+    a.target = "_blank";
+
+    let project = document.createElement("article");
+    project.classList.add("project");
+
+    let img = document.createElement("img")
+    img.src = data["image"];
+
+    let h3 = document.createElement("h3");
+    h3.innerText = data["title"];
+
+    project.appendChild(img);
+    project.appendChild(h3);
+
+    a.appendChild(project);
+
+    projects.appendChild(a);
+}
+
+
+function getProjectsRequest(projects) {
+    fetch(apiEndpoint, {
+        method: "GET",
+        headers: {
+            Accept: 'application/json'
+        }
+    }).then(res => {
+        if (!res.ok) {
+            return;
+        }
+        return res.json();
+    }).then(data => {
+        for (let i=0; i < data.length; i++) {
+            addToProjects(projects, data[i]);
+        }
+    }).catch(error => {
+        return;
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let projects = document.getElementById("projects-body");
+    getProjectsRequest(projects);
+});
